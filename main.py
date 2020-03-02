e9388ad57977e6c10cfea6d971f4c0a284b2037e
Faisal Wirakusuma <faisal1911@gmail.com>
2020-03-02 20:52:22 +0700

    resolusi konflik

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 print("File main.")
 print("coba ubah nanang")
-print(“wish you were here”)
-print(“ExR”)
+print("wish you were here")
+print("ExR")
 print("Faisal was here")
 
 
 #alianto
 print('alianto')
 
-print(“wish you were here”)
-print(“ExR”)
+print("wish you were here")
+print("ExR")
 
 print("Faisal was here")
 
 print("adit")
-print(“wish you were here”)
-print(“ExR”)
+print("wish you were here")
+print("ExR")
 print("Faisal was here")
