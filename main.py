91c6b32bef88e8fbf0a9650ef6b26bef155d0bb4
unknown <oktavianlintin93@gmail.com>
2020-03-02 20:46:43 +0700

    Heboh Virus main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 print("File main.")
-<<<<<<< HEAD
 print("Virus Corona Heboh")
-=======
-
-<<<<<<< HEAD
-print(“wish you were here”)
-print(“ExR”)
-=======
+print("wish you were here”)
+print("ExR")
 print("Faisal was here")
->>>>>>> 6894ffa0d0b87c0a244accd98a4fedd9baddfb0f
->>>>>>> 1537371a108ae834ed8e29e1b136c32dc550479f
+
